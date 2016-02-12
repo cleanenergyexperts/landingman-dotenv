ecf8694a44061211eedceb950fb08cec2b0e6393
Matt Snider <matt@cleanenergyexperts.com>
2016-02-12 09:02:40 -0800

    Allow a .env in the parent directory

--- lib/landingman-dotenv/extension.rb
+++ lib/landingman-dotenv/extension.rb
@@ -15,16 +15,22 @@ module Landingman
       # Properly Scope the paths
       specific_env_path = "#{options.env_file}#{options.env_separator}#{app.environment}"
       global_env_path = options.env_file
+      parent_specific_env_path = File.expand_path(File.join(app.root, '..', specific_env_path))
+      parent_global_env_path = File.expand_path(File.join(app.root, '..', global_env_path))
 
       # Dot Env
       ::Dotenv.load(
         File.join(Dir.pwd, specific_env_path),
-        File.join(Dir.pwd, global_env_path)
+        File.join(Dir.pwd, global_env_path),
+        parent_specific_env_path,
+        parent_global_env_path
       )
       app.before do
         ::Dotenv.load(
           File.join(app.root, specific_env_path),
-          File.join(app.root, global_env_path)
+          File.join(app.root, global_env_path),
+          parent_specific_env_path,
+          parent_global_env_path
         )
       end
     end
